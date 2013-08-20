cc3dbe3a158d3495d666ede5fec3057ca6a9e762
spuds <spuds@spudsdesign.com>
2013-08-20 19:36:48 -0400

    ! move sp whos on-line actions to hook .. piss poor doing it this way, do I DARE say we need another hook here?

--- subs/PortalIntegration.subs.php
+++ subs/PortalIntegration.subs.php
@@ -159,3 +159,83 @@ function sp_integrate_load_permissions(&$permissionGroups, &$permissionList, &$l
 
 	$permissionGroups['membergroup'][] = 'sp';
 }
+
+function sp_integrate_whos_online($actions)
+{
+	global $scripturl, $modSettings, $txt;
+
+	$db = database();
+	$data = null;
+
+	// This may miss if its needed for the first action ... need to improve the hook or find another location
+	if ($modSettings['sp_portal_mode'] == 1)
+	{
+		$txt['who_index'] = sprintf($txt['sp_who_index'], $scripturl);
+		$txt['whoall_forum'] = sprintf($txt['sp_who_forum'], $scripturl);
+	}
+
+	// If its a portal action, lets check it out.
+	if (isset($actions['page']))
+	{
+		$data = $txt['who_hidden'];
+		$page_id = $actions['page'];
+
+		// Due to the way the who action hook works, this is done one by one instead of batch
+		// Arrays are left in place for the future, meaning a new hook is needed
+		$numeric_ids = array();
+		$string_ids = array();
+		$page_where = array();
+
+		if (is_numeric($page_id))
+			$numeric_ids[] = (int) $page_id;
+		else
+			$string_ids[] = $page_id;
+
+		if (!empty($numeric_ids))
+			$page_where[] = 'id_page IN ({array_int:numeric_ids})';
+
+		if (!empty($string_ids))
+			$page_where[] = 'namespace IN ({array_string:string_ids})';
+
+		$result = $db->query('', '
+			SELECT id_page, namespace, title, permission_set, groups_allowed, groups_denied
+			FROM {db_prefix}sp_pages
+			WHERE ' . implode(' OR ', $page_where) . '
+			LIMIT {int:limit}',
+			array(
+				'numeric_ids' => $numeric_ids,
+				'string_ids' => $string_ids,
+				'limit' => count($page_ids),
+			)
+		);
+		$page_data = array();
+		while ($row = $db->fetch_assoc($result))
+		{
+			if (!sp_allowed_to('page', $row['id_page'], $row['permission_set'], $row['groups_allowed'], $row['groups_denied']))
+				continue;
+
+			$page_data[] = array(
+				'id' => $row['id_page'],
+				'namespace' => $row['namespace'],
+				'title' => $row['title'],
+			);
+		}
+		$db->free_result($result);
+
+		if (!empty($page_data))
+		{
+			foreach ($page_data as $page)
+			{
+				if (isset($page_ids[$page['id']]))
+					foreach ($page_ids[$page['id']] as $k => $session_text)
+						$data = sprintf($session_text, $page['id'], censorText($page['title']), $scripturl);
+
+				if (isset($page_ids[$page['namespace']]))
+					foreach ($page_ids[$page['namespace']] as $k => $session_text)
+						$data = sprintf($session_text, $page['namespace'], censorText($page['title']), $scripturl);
+			}
+		}
+	}
+	
+	return $data;
+}
