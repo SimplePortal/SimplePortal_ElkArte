a34f6cfc76daa5240241e90db47b1ae18825e429
spuds <spuds@spudsdesign.com>
2013-08-27 22:11:49 -0400

    ! fix where the directories are getting installed ! wrong integration area ! couple of unused globals

--- database.php
+++ database.php
@@ -18,7 +18,7 @@ if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('ELK'))
 elseif (!defined('ELK'))
 	die('<b>Error:</b> Cannot install - please verify you put this in the same place as ELK\'s index.php.');
 
-global $db_prefix, $modSettings, $db_package_log, $package_cache;
+global $db_prefix, $db_package_log;
 
 $db = database();
 $db_table = db_table();
