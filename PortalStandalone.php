2c95f74732634ebe14f74b6e39c6a428ef886e79
spuds <spuds@spudsdesign.com>
2014-07-07 09:07:07 -0400

    ! work on standalone mode

--- PortalStandalone.php
+++ PortalStandalone.php
@@ -8,27 +8,50 @@
  * @license BSD 3-clause
  *
  * @version 2.4
+ *
+ * 	This file here, unbelievably, has your portal within.
+ *
+ * In order to use SimplePortal in standalone mode:
+ * - Go to "SPortal Admin" >> "Configuration" >> "General Settings"
+ * - Select "Standalone" mode as "Portal Mode"
+ * - Set "Standalone URL" as the full url of this file.
+ * - Edit path to the forum ($forum_dir) in this file.
+ *
+ * See? It's just magic!
+ *
  */
-
 global $sp_standalone;
 
+// Should be the full path!
 $forum_dir = 'full/path/to/forum';
+
+// Let them know the mode.
 $sp_standalone = true;
 
+// Hmm, wrong forum dir?
 if (!file_exists($forum_dir . '/index.php'))
 	die('Wrong $forum_dir value. Please make sure that the $forum_dir variable points to your forum\'s directory.');
 
+// Get out the forum's Elkarte version number.
 $data = substr(file_get_contents($forum_dir . '/index.php'), 0, 4096);
 if (preg_match('~\*\s@version\s+(.+)[\s]{2}~i', $data, $match))
 	$forum_version = 'ElkArte ' . $match[1];
 
+// Load the SSI magic.
 require_once($forum_dir . '/SSI.php');
-require_once(SOURCEDIR . '/PortalMain.php');
 
-if (WIRELESS)
-	redirectexit();
+// Its all about the blocks
+require_once(SUBSDIR . '/PortalBlocks.subs.php');
 
+// Initialize SP in standalone mode mode
+loadTemplate('Portal', 'portal');
 sportal_init(true);
-sportal_main();
 
+// Articles
+require_once(CONTROLLERDIR . '/PortalMain.controller.php');
+$controller = new Sportal_Controller();
+$controller->pre_dispatch();
+$controller->action_index();
+
+// Here we go!
 obExit(true);
